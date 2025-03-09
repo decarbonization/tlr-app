85fb83229e93669966cbb5ad0e6218a3da33553a
P. Kevin Contreras <kevin.macwhinnie@me.com>
2025-03-09 15:44:38 -0400

    plugin settings layout less broken

--- M9r/Settings/PluginSettingsView.swift
+++ M9r/Settings/PluginSettingsView.swift
@@ -24,42 +24,45 @@ struct PluginSettingsView: View {
     
     var body: some View {
         @Bindable var pluginList = Plugin.List.installed
-        
-        HStack {
-            VStack(alignment: .leading) {
+        VStack(alignment: .leading) {
+            HStack {
                 List(pluginList.all, selection: $selection) { plugin in
                     Text(verbatim: plugin.manifest.shortName ?? plugin.manifest.name)
                 }
                 .listStyle(.bordered)
-                Button("Add", systemImage: "plus") {
-                    isShowingImporter = true
-                }
-                .buttonStyle(.accessoryBar)
-                .labelStyle(.iconOnly)
-                .fileImporter(isPresented: $isShowingImporter, allowedContentTypes: [.bundle]) { result in
-                    do {
-                        let importURL = try result.get()
-                        guard importURL.startAccessingSecurityScopedResource() else {
-                            throw CocoaError(.fileReadNoPermission)
+                .frame(width: 200)
+                
+                VStack(alignment: .leading) {
+                    Form {
+                        if let selectedPlugin = pluginList.all.first(where: { $0.id == selection }) {
+                            @Bindable var selectedPlugin = selectedPlugin
+                            Toggle("Enabled", isOn: $selectedPlugin.isEnabled)
                         }
-                        defer {
-                            importURL.stopAccessingSecurityScopedResource()
-                        }
-                        try Plugin.List.installed.add(byCopying: importURL)
-                    } catch {
-                        TaskErrors.all.present(error)
                     }
+                    .padding()
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .backgroundStyle(.regularMaterial)
+                .border(.tertiary, width: 1.0)
+            }
+            
+            Button("Install") {
+                isShowingImporter = true
+            }
+            .fileImporter(isPresented: $isShowingImporter, allowedContentTypes: [.bundle]) { result in
+                do {
+                    let importURL = try result.get()
+                    guard importURL.startAccessingSecurityScopedResource() else {
+                        throw CocoaError(.fileReadNoPermission)
+                    }
+                    defer {
+                        importURL.stopAccessingSecurityScopedResource()
+                    }
+                    try Plugin.List.installed.add(byCopying: importURL)
+                } catch {
+                    TaskErrors.all.present(error)
                 }
             }
-            .frame(width: 200)
-            Form {
-                if let selectedPlugin = pluginList.all.first(where: { $0.id == selection }) {
-                    @Bindable var selectedPlugin = selectedPlugin
-                    Toggle("Enabled", isOn: $selectedPlugin.isEnabled)
-                }
-            }
-            .frame(width: 300)
         }
-        .padding()
     }
 }
