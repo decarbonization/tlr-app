000f3b74631b4f1d530ad8bffcf19373c4e7a414
P. Kevin Contreras <kevin.macwhinnie@me.com>
2025-03-25 11:09:29 -0400

    add tester extension

---
+++ ExtensionTester/ExtensionTester.swift
@@ -0,0 +1,44 @@
+/*
+ * M9r
+ * Copyright (C) 2025  MAINTAINERS
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import ListeningRoomExtensionSDK
+import ExtensionFoundation
+import ExtensionKit
+import SwiftUI
+
+@main struct ExtensionTester: ListeningRoomExtension {
+    var features: [ListeningRoomExtensionFeature] {
+        [
+            .sidebarSection(localizedTitle: "Extension Tester", items: [
+                ListeningRoomExtensionFeatureLink(sceneID: "play-queue",
+                                                  localizedTitle: "Play Queue",
+                                                  image: .systemImage(name: "music.note.list"))
+            ])
+        ]
+    }
+    
+    var body: some AppExtensionScene {
+        ListeningRoomExtensionScene(id: "play-queue") {
+            VStack {
+                Label("Play Queue", systemImage: "music.note.list")
+                    .font(.largeTitle)
+                    .foregroundStyle(.tertiary)
+            }
+        }
+    }
+}
